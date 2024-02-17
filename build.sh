da8ecfc20c78c8595d45884b5dc7569ef1573e46
Hani K <htkhader@gmail.com>
2024-02-17 16:09:03 +0100

    Add module builder to build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ K_ROOT_DIR=$(pwd)
 KERNEL_DEFCONFIG=alioth_nethunter_defconfig
 ANYKERNEL3_DIR=$K_ROOT_DIR/AnyKernel3/
 FINAL_KERNEL_ZIP=Bifr0st_Alioth_1.0.zip
+MODULES_OUT_DIR=$K_ROOT_DIR/out/MODULES_OUT
+RESULT_FOLDER=$K_ROOT_DIR/../../android/Bifr0st_out
 export ARCH=arm64
 
 # Speed up build process
@@ -20,6 +22,7 @@ nocol='\033[0m'
 echo -e "$yellow**** Cleaning ****$nocol"
 mkdir -p out
 make O=out clean
+mkdir -p $MODULES_OUT_DIR
 
 echo -e "$yellow**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****$nocol"
 echo -e "$blue***********************************************"
@@ -31,7 +34,17 @@ make -j$(nproc --all) O=out \
                       CC=$K_ROOT_DIR/../../android/toolchains/prebuilts_clang-r445002/bin/clang \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE=$K_ROOT_DIR/../../android/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android- \
-                      CROSS_COMPILE_ARM32=$K_ROOT_DIR/../../android/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+                      CROSS_COMPILE_ARM32=$K_ROOT_DIR/../../android/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- 
+
+echo -e "$yellow**** Setting up directories ****$nocol"
+if [ ! -d "$RESULT_FOLDER" ]; then
+    echo "Creating $RESULT_FOLDER"
+    mkdir -p "$RESULT_FOLDER"
+fi
+
+TARGET_SUBFOLDER="$RESULT_FOLDER/$(date '+%Y%m%d_%H%M%S')"
+echo "Creating subfolder: $TARGET_SUBFOLDER"
+mkdir -p "$TARGET_SUBFOLDER"
 
 echo -e "$yellow**** Verify Image.gz-dtb & dtbo.img ****$nocol"
 ls $K_ROOT_DIR/out/arch/arm64/boot/Image.gz-dtb
@@ -48,10 +61,15 @@ echo -e "$yellow**** Copying Image.gz-dtb & dtbo.img ****$nocol"
 cp $K_ROOT_DIR/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
 cp $K_ROOT_DIR/out/arch/arm64/boot/dtbo.img $ANYKERNEL3_DIR/
 
+echo -e "$yellow**** Installing modules ****$nocol"
+make modules_install INSTALL_MOD_PATH=$MODULES_OUT_DIR O=out
+echo "Copying MODULES_OUT to $TARGET_SUBFOLDER"
+cp -r "$MODULES_OUT_DIR" "$TARGET_SUBFOLDER"
+
 echo -e "$yellow**** Time to zip up! ****$nocol"
 cd $ANYKERNEL3_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
-cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP $K_ROOT_DIR/../../android/$FINAL_KERNEL_ZIP
+cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP $TARGET_SUBFOLDER
 
 echo -e "$yellow**** Done, here is your checksum ****$nocol"
 cd ..
@@ -63,4 +81,4 @@ rm -rf out/
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
-sha1sum $K_ROOT_DIR/../../android/$FINAL_KERNEL_ZIP
+sha1sum $TARGET_SUBFOLDER/$FINAL_KERNEL_ZIP
